4485ada19e0065bd52b94d3f46b75e187b36b07b
web-codeman <pastucha.lukasz@gmail.com>
2017-04-06 23:44:57 +0200

    #2 Wyrażenia regularne
    -dodano obsługę poprawności adresu IP

--- IpAndEmailChecker/IpAndEmailChecker/Program.cs
+++ IpAndEmailChecker/IpAndEmailChecker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,66 @@ namespace IpAndEmailChecker
 {
     class Program
     {
+        String ipAddress;
+        String email;
+
+        public Program()
+        {
+            setIpAddress();
+            setEmail();
+        }
+
+        private void setIpAddress()
+        {
+            Console.WriteLine("Podaj IP: ");
+            ipAddress = Console.ReadLine();
+            checkIpAddress();
+        }
+
+        private void checkIpAddress()
+        {
+            String ipPattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"; ;
+            if (Regex.IsMatch(ipAddress,ipPattern))
+            {
+                Console.WriteLine("IP address ok!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong IP address");
+                setIpAddress();
+            }
+        }
+
+        private void setEmail()
+        {
+            Console.WriteLine("Podaj Email: ");
+            email = Console.ReadLine();
+            checkEmail();
+        }
+
+        private void checkEmail()
+        {
+            if (Regex.IsMatch(email,""))
+            {
+                Console.WriteLine("email ok!");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Wrong email");
+                setEmail();
+            }
+        }
+
+
+
+
         static void Main(string[] args)
         {
+            new Program();
         }
     }
 }
